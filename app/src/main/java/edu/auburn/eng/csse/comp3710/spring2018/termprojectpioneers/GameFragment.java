16b427f3eaeee744fb11458720c15ab44a331d2f
willj <willjacksonau@gmail.com>
2018-05-01 13:59:18 -0500

    Sound Buttons.

--- app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/termprojectpioneers/GameFragment.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/termprojectpioneers/GameFragment.java
@@ -9,6 +9,9 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -58,6 +61,14 @@ public class GameFragment extends Fragment {
     private TextView mScoreView;
     private TextView mHighScoreView;
     private TextView mMessageView;
+
+    private AudioManager mAudioManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -512,6 +523,24 @@ public class GameFragment extends Fragment {
         super.onDestroy();
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_sound_list, menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.sound_off:
+                mAudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_MUTE, 0);
+                return true;
+            case R.id.sound_up:
+                mAudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_RAISE, 0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
